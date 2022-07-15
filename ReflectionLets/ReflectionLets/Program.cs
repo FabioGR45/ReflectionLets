92e17266a3f625daa6333dca1c2e20c43c385f20
Fábio Garcia Rios <fabiogarcia45@gmail.com>
2022-07-14 23:26:42 -0300

    Adicionando projeto de Reflection ao repositório
    Adicionada a pasta contendo o projeto Lets Speak final, testado e corrigido, com 2 tipos diferentes de representação do reflection

---
+++ ReflectionLets/ReflectionLets/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Reflection;
+
+namespace ReflectionLets
+{
+    public class Program
+    {
+        static void DisplayPublicProperties(object obj)
+        {
+
+            var tipo = obj.GetType();
+            var builder = new StringBuilder();
+
+            foreach(var p in tipo.GetProperties()) {
+            builder.AppendLine(p.Name + ": " + p.GetValue(obj));
+            }
+
+            Console.WriteLine(builder);
+        }
+
+        public static void CreateInstance()
+        {
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (var type in types)
+            {
+                if (type.IsAssignableTo(typeof(Student)) && type.IsClass && !type.IsAbstract)
+                {
+                    var instance = (Student)Activator.CreateInstance(type);
+                    var name = instance.Name;
+                    var university = instance.University;
+                    var number = instance.RollNumber;
+
+                    var newStudent = new Student("Fábio", "FMU", 12345);
+                    DisplayPublicProperties(newStudent);
+                    newStudent.DisplayInfo();
+                }
+            }
+
+
+        }
+
+        static void Main() {
+
+            //Estilo 1
+            var aluno1 = new Student() { Name = "Fábio", University = "FMU", RollNumber = 12345 };
+            var aluno2 = new Student() { Name = "Lucio", University = "Uninove", RollNumber = 67890 };
+
+            DisplayPublicProperties(aluno1);
+
+            //Estilo 2
+            CreateInstance();
+        }
+
+    }
+
+}
---
+++ ReflectionLets/ReflectionLets/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionLets
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string University { get; set; }
+        public int RollNumber { get; set; }
+
+        public Student()
+        {
+
+        }
+        public Student(string Name, string University, int number)
+        {
+            this.Name = Name;
+            this.University = University;
+            this.RollNumber = number;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"{Name} - {University} - {RollNumber}");
+        }
+    }
+
+}
